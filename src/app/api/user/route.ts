b247e611129798c29e4d59cf2d543944f464f987
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2023-03-28 16:41:10 +0700

    [TEST] tested prisma query

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,6 +3,19 @@ import prisma from "../prisma/prisma";
 
 export async function GET(req: NextRequest) {
   console.log(req.nextUrl);
-  let user = await prisma.user.findMany({});
-  return NextResponse.json({ user: user });
+  let user = await prisma.user.findMany({
+    select: {
+      id: true,
+      username: true,
+      firstName: true,
+      lastName: true,
+      password: false,
+      email: true,
+      phoneNo: true,
+      avatar: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+  return NextResponse.json(user, { status: 200 });
 }

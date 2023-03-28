8fc2b10399d786aab9a066546c0881fc56ca0434
ThuPham02 <63948923+anhthu106@users.noreply.github.com>
2023-03-28 23:25:49 +0700

    Update index.tsx

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,41 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Button from "../Button";
-const images = [
-  {
-    id: 1,
-    name: "[Sai Gon] Tren Nhung Dam May - Chillies Concert Tour",
-    place: "SEC District 7, Ho Chi Minh City",
-    time: "16:00 - 22:00 | 20/05/2023",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere lorem sit amet quam posuere tempor. Sed dui felis, dignissim ac ante quis, faucibus ullamcorper enim. Nulla tincidunt rhoncus erat, vel cursus odio consequat eget. Ut interdum semper ipsum eu gravida. Duis risus nulla, interdum id elit nec, auctor semper metus. Maecenas lacinia, erat ut ultrices iaculis, libero velit condimentum tellus, sit amet ultrices diam ex eget tortor. In quis lacinia risus, eget porta erat. Quisque vestibulum tincidunt neque, id luctus ante. Praesent lacinia arcu sit amet facilisis mollis. Mauris cursus ut quam eu sodales.",
-    price: 550000,
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png",
-  },
-  {
-    id: 2,
-    name: "[Ha Noi] Tren Nhung Dam May - Chillies Concert Tour",
-    place: "SEC District 7, Ho Chi Minh City",
-    time: "16:00 - 22:00 | 20/05/2023",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere lorem sit amet quam posuere tempor. Sed dui felis, dignissim ac ante quis, faucibus ullamcorper enim. Nulla tincidunt rhoncus erat, vel cursus odio consequat eget. Ut interdum semper ipsum eu gravida. Duis risus nulla, interdum id elit nec, auctor semper metus. Maecenas lacinia, erat ut ultrices iaculis, libero velit condimentum tellus, sit amet ultrices diam ex eget tortor. In quis lacinia risus, eget porta erat. Quisque vestibulum tincidunt neque, id luctus ante. Praesent lacinia arcu sit amet facilisis mollis. Mauris cursus ut quam eu sodales.",
-    price: 550000,
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png",
-  },
-  {
-    id: 3,
-    name: "[Da Nang] Tren Nhung Dam May - Chillies Concert Tour",
-    place: "SEC District 7, Ho Chi Minh City",
-    time: "16:00 - 22:00 | 20/05/2023",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere lorem sit amet quam posuere tempor. Sed dui felis, dignissim ac ante quis, faucibus ullamcorper enim. Nulla tincidunt rhoncus erat, vel cursus odio consequat eget. Ut interdum semper ipsum eu gravida. Duis risus nulla, interdum id elit nec, auctor semper metus. Maecenas lacinia, erat ut ultrices iaculis, libero velit condimentum tellus, sit amet ultrices diam ex eget tortor. In quis lacinia risus, eget porta erat. Quisque vestibulum tincidunt neque, id luctus ante. Praesent lacinia arcu sit amet facilisis mollis. Mauris cursus ut quam eu sodales.",
-    price: 550000,
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png",
-  },
-];
+import { IService } from "../Service/serviceInterface";
+import { services } from "@/data/service";
 
 const Carousel = () => {
   const [currentSlide, setCurrentSlide] = useState(0);

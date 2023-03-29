5a664d614c0ae5945d109b56f8920ad46766c2d0
Pham Vo Dong <phamvodong0811@gmail.com>
2023-03-30 01:06:32 +0700

    [Modify] Modify UX/UI login and signup page

---
+++ src/app/(authenticate)/layout.tsx
@@ -0,0 +1,12 @@
+import Logo from "components/NavBar/logo";
+
+export default function AuthenticateLayout({children}: { children: React.ReactNode }) {
+    return (
+        <section className={"h-screen bg-authenticate bg-oliveGreen py-8 px-4 sm:py-14"}>
+            <div className="flex flex-col items-center justify-center">
+                <Logo/>
+                 {children}
+            </div>
+
+        </section>)
+}

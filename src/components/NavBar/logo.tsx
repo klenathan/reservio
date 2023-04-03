54b5295a32db9a970fcc4123998a189fd6a54f32
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2023-04-03 12:25:25 +0700

    [FEAT] update Logo component

--- src/components/NavBar/logo.tsx
+++ src/components/NavBar/logo.tsx
@@ -1,15 +1,25 @@
 import Link from "next/link";
 
-export default function Logo() {
+interface ILogoStyle {
+  logoStyle: "green" | "white";
+}
+
+const Logo: React.FC<ILogoStyle> = (props: ILogoStyle) => {
   return (
     <Link
       href={"/"}
-      className="sm:text-left flex flex-col cursor-pointer max-w-screen-xl"
+      className="sm:text-left md:text-center flex flex-col cursor-pointer max-w-screen-xl"
     >
-      <p className="font-logo font-semibold text-5xl italic text-white ">
+      <p
+        className={`font-logo font-semibold text-5xl italic ${
+          props.logoStyle == "green" ? "text-midGreen" : "text-white"
+        } `}
+      >
         Reservio
       </p>
       <p className="text-base italic text-white-500">Reserve it? Reservio</p>
     </Link>
   );
-}
+};
+
+export default Logo;

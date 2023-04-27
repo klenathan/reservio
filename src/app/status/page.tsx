8c127b929fa52eb8faa9cb258beef93c51f78cf5
HaBui7 <92909964+HaBui7@users.noreply.github.com>
2023-04-27 22:44:32 +0700

    fix calendar

---
+++ src/app/status/page.tsx
@@ -0,0 +1,19 @@
+
+import NavBar from "@/components/NavBar";
+import Head from "next/head";
+
+
+export default function Profile() {
+  return (
+    <div>
+      <Head>
+        <title>Reservation status</title>
+      </Head>
+      {/* <NavBar> </NavBar> */}
+      <div>
+        
+      </div>
+
+      </div>
+  );
+}

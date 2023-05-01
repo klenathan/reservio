12b6793019a5d7bdb744f63d9e5e933a74c8772e
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2023-05-02 03:03:30 +0700

    [feat] implemented react-table

--- src/app/(main)/detail/[id]/page.tsx
+++ src/app/(main)/detail/[id]/page.tsx
@@ -1,57 +1,57 @@
 "use client";
 import Breadcrumb from "components/Breadcrumb";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import DetailPage from "components/Detail";
-import {NotFound} from "next/dist/client/components/error";
+import { NotFound } from "next/dist/client/components/error";
 import apiClient from "@/config/axios.config";
-import {Product} from "../../../../../Types";
+import { Product } from "../../../../../Types";
 import LoadingSpinner from "components/LoadingSpinner";
 
 interface DetailParams {
-    params: {
-        id: string;
-    };
+  params: {
+    id: string;
+  };
 }
 
 export default function Detail(slugs: DetailParams) {
-    const [product, setProduct] = useState<Product>();
-    // const [isLoading, setIsLoading] = useState(true)
-    const [isError, setIsError] = useState(false);
+  const [product, setProduct] = useState<Product>();
+  // const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false);
 
-    const items = [
-        {label: "Home", href: "/"},
-        {label: product?.category, href: `/category?id=${product?.category}`},
-        {label: product?.name, href: "/"},
-    ];
+  const items = [
+    { label: "Home", href: "/" },
+    { label: product?.category, href: `/category?id=${product?.category}` },
+    { label: product?.name, href: "/" },
+  ];
 
-    // TODO: Optimize the page loading and not found
-    useEffect(() => {
-        apiClient
-            .get(`service/${slugs.params.id}`)
-            .then((res) => {
-                setProduct(res.data);
-            })
-            .catch((e) => {
-                setIsError(true);
-            });
-    }, [slugs.params.id]);
+  // TODO: Optimize the page loading and not found
+  useEffect(() => {
+    apiClient
+      .get(`service/${slugs.params.id}`)
+      .then((res) => {
+        setProduct(res.data);
+      })
+      .catch((e) => {
+        setIsError(true);
+      });
+  }, [slugs.params.id]);
 
-
-    if (!product && !isError) {
-        return <div
-            className="relative h-[calc(100vh_-_10rem)] -top-[5rem] w-full flex flex-col justify-center items-center overflow-hidden -z-10">
-            <LoadingSpinner text="Loading product data, please wait..."/>
-        </div>;
-    }
-
-    if (isError || !product) {
-        return <NotFound/>;
-    }
-
+  if (!product && !isError) {
     return (
-        <div className="h-full px-4 md:px-8 lg:px-40 2xl:px-56">
-            <Breadcrumb items={items}/>
-            <DetailPage service={product}/>
-        </div>
+      <div className="relative h-[calc(100vh_-_10rem)] -top-[5rem] w-full flex flex-col justify-center items-center overflow-hidden -z-10">
+        <LoadingSpinner text="Loading product data, please wait..." />
+      </div>
     );
+  }
+
+  if (isError || !product) {
+    return <NotFound />;
+  }
+
+  return (
+    <div className="h-full px-4 md:px-8 lg:px-40 2xl:px-56">
+      <Breadcrumb items={items} />
+      <DetailPage service={product} />
+    </div>
+  );
 }
--- src/components/Admin/table.tsx
+++ src/components/Admin/table.tsx
@@ -1,5 +1,9 @@
+import apiClient from "@/config/axios.config";
 import axios from "axios";
+import React from "react";
 import { useEffect, useState } from "react";
+import { Column, useTable } from "react-table";
+import { User } from "../../../Types";
 
 interface Vendor {
   id: string;
@@ -7,23 +11,145 @@ interface Vendor {
 
 interface IUser {
   id: string;
-  avatar?: string;
+  avatar: string;
+  createdAt: string;
   email: string;
+  firstName: string;
+  lastName: string;
   phoneNo: string;
   status: string;
+  updatedAt: string;
   username: string;
-  vendor?: Vendor;
+  vendor: Vendor | null;
 }
+
+const TableComponent = (props: { data: IUser[] }) => {
+  const columns: Column<IUser>[] = React.useMemo(
+    () =>
+      [
+        {
+          Header: "id",
+          accessor: "id",
+        },
+        {
+          Header: "Username",
+          accessor: "username",
+        },
+        {
+          Header: "Email",
+          accessor: "email",
+        },
+        {
+          Header: "name",
+          accessor: "firstName",
+        },
+        {
+          Header: "Phone",
+          accessor: "phoneNo",
+        },
+        {
+          Header: "Status",
+          accessor: "status",
+        },
+        {
+          Header: "Create date",
+          accessor: "createdAt",
+        },
+        {
+          Header: "Updated date",
+          accessor: "updatedAt",
+        },
+        {
+          Header: "Vendor",
+          accessor: "vendor",
+          Cell: ({ cell: { value } }) => {
+            return value != null ? (
+              <p className="font-semibold text-green-400">True</p>
+            ) : (
+              <p className="font-semibold text-red-400">False</p>
+            );
+          },
+        },
+        {
+          width: 300,
+          Header: "Action",
+          Cell: (cell: any) => (
+            <button
+            className="underline font-semibold hover:text-red-400"
+              onClick={() => {
+                console.log("clicked", cell.row.values.id);
+              }}
+            >
+              Deactivate
+            </button>
+          ),
+        },
+      ] as Column<IUser>[],
+    []
+  );
+  const data = props.data;
+  const tableInstance = useTable({ columns, data: data });
+  const rows = tableInstance.rows;
+
+  return (
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        {tableInstance.headerGroups.map((headerGroup) => (
+          <tr key={headerGroup.getHeaderGroupProps().key}>
+            {headerGroup.headers.map((column) => (
+              <th className="px-6 py-3" key={column.getHeaderProps().key}>
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {rows.map((row, i) => {
+          tableInstance.prepareRow(row);
+          return (
+            <tr
+              className="bg-white border-b dark:bg-gray-900 dark:border-gray-700
+              hover:bg-green-100"
+              key={row.getRowProps().key}
+            >
+              {row.cells.map((cell) => {
+                // if (cell.column.id == "vendor") {
+                //   cell.value =
+                //     cell.value != null ? (
+                //       <p className="font-semibold text-green-400">True</p>
+                //     ) : (
+                //       <p className="font-semibold text-red-400">False</p>
+                //     );
+                // }
+                let cell_k = cell.getCellProps().key;
+                return (
+                  <td className="px-6 py-4" key={cell_k}>
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 const Table = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(`https://06ufwajgc6.execute-api.ap-southeast-1.amazonaws.com/user`)
+    apiClient
+      .get(`/user`)
       .then((response) => {
         setUsers(response.data);
+      })
+      .catch((e) => {
+        console.log(e.response.data);
       });
+    //
   }, []);
 
   return (
@@ -31,54 +157,7 @@ const Table = () => {
       <h1 className="w-full text-xl text-oliveGreen font-bold uppercase pl-4">
         All Accounts:
       </h1>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Username
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Avatar
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Email
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Phone number
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Vendor
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Status
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user: IUser) => {
-              return (
-                <tr
-                  key={user.id}
-                  className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
-                >
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    {user.username}
-                  </th>
-                  <td className="px-6 py-4">{user.avatar}</td>
-                  <td className="px-6 py-4">{user.email}</td>
-                  <td className="px-6 py-4">{user.phoneNo}</td>
-                  <td className="px-6 py-4">{user.vendor ? "Yes" : "No"}</td>
-                  <td className="px-6 py-4">{user.status}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      {users.length > 0 && <TableComponent data={users} />}
     </div>
   );
 };

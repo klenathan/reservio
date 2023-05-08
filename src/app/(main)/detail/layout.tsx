4e8059ff45001ccb4fbdf065b2662729871692c5
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2023-05-08 19:04:02 +0700

    [FEAT] :frog: added Logo

---
+++ src/app/(main)/detail/layout.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+
+export const metadata = {
+    title: "Product Detail",
+    description: "Reserve it? Reservio",
+};
+
+
+export default function Layout({ children }: any) {
+  return <>{children}</>;
+}
